59e8c07841302686957db8278c587591e63dd68a
Daniel R. Rudolph <daniel.rudolph@caesar.de>
2018-03-29 18:15:10 +0200

    more functions

--- frame_archiver.py
+++ frame_archiver.py
@@ -9,12 +9,21 @@ import logging
 #import bagit
 
 
-class FrameDataset:
-    def __init__(self, path, uid, gid):
-        self.path = path
-        self.uid = uid
-        self.gid = gid
 
+TAPEDEVICE = "/dev/tape/by-id/scsi-3500507631211505c-nst"
+CHANGERDEVICE = "/dev/tape/by-id/scsi-3500e09efff107510"
+LTFSMOUNTPOINT = "/mnt/ltfs-archive/"
+INDEXFILESDIR = "/root/TAPES/"
+MAILADDRESS = ""
+
+
+
+class FrameDataset:
+    def __init__(self, path):
+        self.path = path
+        self.uid = 0
+        self.gid = 0
+        self.tapes = []
 
 def adjust_dir_permissions(path):
     '''change user and permissions according to username in dir name'''
@@ -34,13 +43,119 @@ def adjust_dir_permissions(path):
             os.chown(fname, uid, gid)
             os.chmod(fname, 0o640)
 
+
+
+def yes_or_no(question):
+    reply = str(input(question+' (y/n): ')).lower().strip()
+    if reply[0] == 'y':
+        return True
+    if reply[0] == 'n':
+        return False
+#    else:
+#        return yes_or_no("please enter y or n")
+
+def mail_message(message):
+    
+    email.message_from_string(message)
+    msg['Subject'] = 'testmail with python'
+    msg['From'] = 'python@ema3013'
+    msg['To'] = MAILADDRESS
+    s = smtplib.SMTP('localhost')
+    s.send_message(msg)
+    s.quit()
+
+
+def script_fail(errormsg="unknown error"):
+    """"""
+    print("script failed with error:", errormsg)
+
+
+def lock_directory():
+    #TODO
+    pass
+
+
+def unlock_directory():
+    #TODO
+    pass
+
+def get_tape_label():
+    """read label of tape in drive"""
+    tapelabel = "ERROR"
+    output = subprocess.check_output(["mtx", "-f", CHANGERDEVICE, "status"])
+    line = output.splitlines()[1]
+
+    #print("type of output:",type(output))
+    #print(output)
+    #print(line)
+
+    tapelabel = line.decode().split()[9]
+    #print(tapelabel)
+    return tapelabel
+
+
+def change_tape(slotnum):
+    """change tape in drive"""
+    result = subprocess.check_call(["mtx", "-f", CHANGERDEVICE, "load", str(slotnum)])
+    if result:
+        script_fail("tape change failed")
+        return result
+    time.sleep(60)
+    #TODO: test if successful
+    return result
+
+
+def format_tape(tape, ask_to_confirm=True):
+    """format tape in drive with LTFS"""
+    #alias mkltfs-loader2='mkltfs --device=/dev/tape/by-id/scsi-3500507631211505c-nst
+    # --rules="size=500K/name=indexfile.txt"'
+    result = False
+    command_path = "/opt/QUANTUM/ltfs/bin/"
+    command = "mkltfs"
+    format_rules = "size=500K/name=indexfile.txt"
+    print(format_rules)
+    #TODO: confirm formatting
+    loaded_tape = get_tape_label()
+    if ask_to_confirm is True:
+        confirm = yes_or_no("FORMAT tape labeled " + loaded_tape + " to LTFS, serial " + tape + "?")
+        if confirm is True:
+            result = subprocess.check_call([command_path+command, "-d",
+                                            TAPEDEVICE, "-r", format_rules, "-s", tape, "-n", tape])
+        else:
+            print("formatting cancelled by user")
+            script_fail(errormsg="formatting cancelled")
+    else:
+        result = subprocess.check_call([command_path+command, "-d",
+                                        TAPEDEVICE, "-r", format_rules, "-s", tape, "-n", tape])
+    print(result)
+    return result 
+
+
+def mount_tape(mountpoint):
+    #alias mountltfs-loader2='/opt/QUANTUM/ltfs/bin/ltfs-singledrive -o devname=/dev/tape/by-id/scsi-3500507631211505c-nst /mnt/ltfs-loader2'
+    result = False
+    command_path = "/opt/QUANTUM/ltfs/bin/"
+    command = "ltfs-singledrive" + ' -o devname=' + TAPEDEVICE + ' ' + mountpoint
+    #TODO: check if mountpoint exits
+    #TODO: check if mountpoint is not already mounted
+
+    result = subprocess.check_call(command_path+command, shell=True)
+    #TODO: check if mountpoint is mounted
+    return mountpoint
+
+
+
+
+
+
+
 #logger = logging.Logger()
 logging.basicConfig(level=logging.DEBUG)
 
 logging.info("starting ...")
 
-frame_dir=pathlib.PosixPath("./TEST-IGNORE").resolve()
-archive_dir=pathlib.PosixPath("./TEST-IGNORE/ARCHIVE").resolve()
+frame_dir = pathlib.PosixPath("./TEST-IGNORE").resolve()
+archive_dir = pathlib.PosixPath("./TEST-IGNORE/ARCHIVE").resolve()
 
 
 #sanity checks
